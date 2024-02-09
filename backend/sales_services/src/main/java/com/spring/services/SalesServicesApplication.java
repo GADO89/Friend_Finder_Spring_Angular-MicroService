e8840f014c5c98efe8cb1da1d3538eda0dc56dba
GADO89 <gadomahmoud377@gmail.com>
2024-02-09 21:16:22 +0200

    create services

---
+++ backend/sales_services/src/main/java/com/spring/services/SalesServicesApplication.java
@@ -0,0 +1,13 @@
+package com.spring.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesServicesApplication.class, args);
+    }
+
+}
