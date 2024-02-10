94a19920b694342093fedb1d42e7a0b5e200aa39
GADO89 <gadomahmoud377@gmail.com>
2024-02-10 20:45:27 +0200

    clean code

--- backend/user-management/src/main/java/com/use/management/config/HibernateConfig.java
+++ backend/user-management/src/main/java/com/use/management/config/HibernateConfig.java
@@ -1,5 +1,8 @@
 package com.use.management.config;
 
+import javax.sql.DataSource;
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,53 +12,49 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import javax.sql.DataSource;
-import java.util.HashMap;
-
 @Configuration
 public class HibernateConfig {
 
     @Value("${datasource.driverClassName}")
-   private String driverClassName;
+    private String driverClassName;
     @Value("${datasource.databaseUrl}")
     private String databaseUrl;
     @Value("${datasource.databaseUser}")
-    private String  databaseUser;
+    private String databaseUser;
     @Value("${datasource.databasePassword}")
-    private String  databasePassword;
+    private String databasePassword;
     @Value("${jpa.hibernate.hibernateDDLAuto}")
-    private String   hibernateDDLAuto;
+    private String hibernateDDLAuto;
     @Value("${jpa.hibernate.hibernateDialect}")
-    private String   hibernateDialect;
+    private String hibernateDialect;
     @Value("${jpa.showSql}")
-    private String   showSql;
+    private String showSql;
     @Value("${jpa.hibernate.create_empty_composites.enable}")
-    private boolean  create_empty_composites;
-
+    private boolean create_empty_composites;
 
     @Bean(name = "platformDataSource")
-    public DataSource platformDataSource(){
+    public DataSource platformDataSource() {
         return DataSourceBuilder.create().username(databaseUser)
-                .password(databasePassword).url(databaseUrl).driverClassName(driverClassName).build();
+                        .password(databasePassword).url(databaseUrl)
+                        .driverClassName(driverClassName).build();
     }
 
     @Bean(name = "entityManagerFactory")
-    public LocalContainerEntityManagerFactoryBean platformEntityManager(){
-       LocalContainerEntityManagerFactoryBean em=
-               new LocalContainerEntityManagerFactoryBean();
-       em.setDataSource(platformDataSource());
-       em.setPackagesToScan(new String[]{"com.use.management"});
-       em.setPersistenceUnitName("platformPersistenceUnitName");
-        HibernateJpaVendorAdapter vendorAdapter=new HibernateJpaVendorAdapter();
-       em.setJpaVendorAdapter(vendorAdapter);
+    public LocalContainerEntityManagerFactoryBean platformEntityManager() {
+        LocalContainerEntityManagerFactoryBean em =
+                        new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(platformDataSource());
+        em.setPackagesToScan(new String[] { "com.use.management" });
+        em.setPersistenceUnitName("platformPersistenceUnitName");
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
 
-        HashMap <String, Object> properties=new HashMap<>();
+        HashMap<String, Object> properties = new HashMap<>();
         properties.put("hibernate.hbm2ddl.auto", hibernateDDLAuto);
         properties.put("hibernate.dialect", hibernateDialect);
         properties.put("hibernate.showSql", showSql);
-        properties.put("${jpa.hibernate.create_empty_composites.enable}", create_empty_composites);
-
-
+        properties.put("${jpa.hibernate.create_empty_composites.enable}",
+                        create_empty_composites);
 
         em.setJpaPropertyMap(properties);
 
@@ -63,9 +62,9 @@ public class HibernateConfig {
     }
 
     @Bean(name = "platformTransactionManager")
-    public PlatformTransactionManager platformTransactionManager(){
+    public PlatformTransactionManager platformTransactionManager() {
 
-        JpaTransactionManager transactionManager=new JpaTransactionManager();
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
         transactionManager.setEntityManagerFactory(platformEntityManager().getObject());
         return transactionManager;
     }
