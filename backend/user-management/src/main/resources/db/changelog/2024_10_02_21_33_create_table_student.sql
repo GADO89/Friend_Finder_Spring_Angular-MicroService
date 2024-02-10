4de45b2e5bb20876653bdd7928d1d5880cde261f
GADO89 <gadomahmoud377@gmail.com>
2024-02-11 00:02:22 +0200

    refactor xml file to sql file

---
+++ backend/user-management/src/main/resources/db/changelog/2024_10_02_21_33_create_table_student.sql
@@ -0,0 +1,6 @@
+
+        CREATE TABLE student
+        (
+            id   bigint NOT NULL PRIMARY KEY,
+            name varchar(255)
+        )
---
+++ backend/user-management/src/main/resources/db/changelog/2024_10_02_22_22_alter_table_student.sql
@@ -0,0 +1,5 @@
+
+        ALTER TABLE student DROP COLUMN name;
+        ALTER TABLE student ADD age varchar(255);
+
+
