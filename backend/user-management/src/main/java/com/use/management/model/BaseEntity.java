3d81e22ba9a18055884927ca352c4a37c55169c5
GADO89 <gadomahmoud377@gmail.com>
2024-02-11 02:46:11 +0200

    55/99 @Audited

--- backend/user-management/src/main/java/com/use/management/model/BaseEntity.java
+++ backend/user-management/src/main/java/com/use/management/model/BaseEntity.java
@@ -2,20 +2,27 @@ package com.use.management.model;
 
 import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import org.hibernate.envers.Audited;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@EqualsAndHashCode(callSuper = false)
 @Data
 @MappedSuperclass
+@Audited
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
